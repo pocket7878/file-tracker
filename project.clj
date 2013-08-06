1e8c84ba9a3f3e953c932d3497671374a528e687
Masato Sogame <poketo7878@gmail.com>
2013-08-06 12:47:38 +0900

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject file-tracker "0.1.0-SNAPSHOT"
+(defproject file-tracker "0.1"
   :description "File & Folder update tracker for clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/pocket7878/file-tracker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
